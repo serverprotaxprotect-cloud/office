addb995156efd2f273f6ffa72e427d4ae6364319
bkgupta5011 <bkgupta5011@gmail.com>
2026-06-03 11:56:30 +0530

    Bust Income Tax client script cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Gee Bharat Attendance Service Worker
-const CACHE_NAME   = 'gee-bharat-attendance-v11';
+const CACHE_NAME   = 'gee-bharat-attendance-v12';
 const OFFLINE_URL  = '/offline.html';
 
 // Static assets to cache immediately on install
